0e31b13b8e83a3b99307c1c8ba549f588da7f972
atomixxx <alejo.loaiza@gmail.com>
2018-04-15 15:34:57 -0500

    [Add] Minor changes

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "go-rest-api/restserver"
+import (
+	"go-rest-api/extra"
+	"go-rest-api/restserver"
+)
 
 func main() {
+	extra.DBConnectPostgres()
 	restserver.CreateRestServer()
 }
