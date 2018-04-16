a37eb3c9dba297ec6ee568c8a6f8b6dcc69cf7ce
atomixxx <alejo.loaiza@gmail.com>
2018-04-16 07:56:10 -0500

    [Add] Changes to handle config

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"go-rest-api/extra"
 	"go-rest-api/restserver"
+	"os"
 )
 
 func main() {
+	arg := "./extra/config.json"
+	if len(os.Args) > 1 {
+		arg = os.Args[1]
+	}
+	_ = extra.GetConfig(arg)
 	extra.DBConnectPostgres()
 	restserver.CreateRestServer()
 }
